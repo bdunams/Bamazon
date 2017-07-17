8e4b724dfca7833a645fd0d9263e1624413448dd
Brian Dunams <dabadestboy1@gmail.com>
2017-07-17 19:58:55 -0400

    Version 1.0

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,34 +1,66 @@
+// Require Bamazon DB Connection
 const Bamazon = require('./connections').Bamazon;
-
+// Require inquirer
 const inquirer = require('inquirer');
 
+// Function to get choice and quantity
+// then complete purchase and update DB
 purchase = () =>{
+  // 
   inquirer.prompt([
   {
     type: 'input',
-    name: 'userChoice',
+    name: 'choice',
     message: 'Please enter the id of the item you wish to purchase.'
   },
   {
     type: 'input',
-    name: 'userQuantity',
+    name: 'quantity',
     message: 'And how may would you like?'
   }
-  ]).then((results)=>{
-    console.log(results);
-  })
+  ]).then((userInput)=>{
+    // Get DB info of the item selected
+    Bamazon.queryAsync('SELECT * FROM products WHERE item_id = ?', [userInput.choice])
+      .then(queryItem => {
+      // if there isn't enough in stock, throw error
+      if(queryItem[0].stock_quantity < userInput.quantity){
+        throw 'Insufficient quantity';
+        return;
+      }
+      // else update stock in db and display purchase
+      else{
+        // UPDATE DB item reflecting purchased quantity
+        Bamazon.queryAsync('UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ? LIMIT 1', [userInput.quantity, userInput.choice])
+          .then(() => {
+          
+          let total = parseInt(userInput.quantity) * queryItem[0].price;
+          
+          console.log(`\nTransaction was successful!\n
+            Your total is ${total}`);
+          
+        })
+      }
+      // Close DB connection
+    }).then(Bamazon.end())
+      // Error with getting DB info
+      .catch((err) => console.log(err));
+    // Error getting user input
+  }).catch((err) => console.log(err));
 }
 
+// Function to connect to DB and display products
 Bamazon.connectAsync().then(() =>{
+  // MySQL query for all from products table
   Bamazon.queryAsync('SELECT * FROM products')
   .then(data => {
+    
     data.forEach((product) =>{
       console.log(
         `${product.item_id} -- ${product.product_name} ---- ${product.price}`
       );
     });
+    // Customer can now purchase
     purchase();
   })
-  .then(Bamazon.end())
 })
 
